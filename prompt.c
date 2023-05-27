ada4b89efa76e5ee3425f83da5359efe39960050
MubarakXBT <kazeemmubarak@gmail.com>
2023-05-27 03:24:53 +0100

    The shell can now exec the EXIT command

--- prompt.c
+++ prompt.c
@@ -6,38 +6,31 @@ void prompt(void)
 {
 	printf("$ ");
 }
-/*#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-**
-char* getlyne()
-{
-	char *buffer;
-	size_t bufsize = 32;
-	ssize_t ch;
-	
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate");
-		exit(1);
-	}
-	ch = getline(&buffer, &bufsize,stdin);
-	return (buffer);
-}
-*/
+
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  */
 int main(void)
 {
+	int res;
 	char* buffer;
+	char q[] = "exit";
+
+
 	while (true)
 	{
 		prompt();
 		buffer = getlyne();
-		parser(buffer);
+		
+		res = strncmp(buffer, q, 4);
+		if (res == 0)
+		{
+			exit(1);
+		}
+		else
+			parser(buffer);
 	}
 	return (0);
 }
