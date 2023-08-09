689034f8dc83138e27dbb488f6efdb2beeb9f232
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-09 03:33:09 +0100

    Fixed the betty errors for all functions in my shell project II

--- main.h
+++ main.h
@@ -2,8 +2,8 @@
 #define _MAIN_H_
 
 void prompt(void);
-char* getlyne();
-char* parser(char* buffer);
-char** arg_token(char* token);
-void exec(char* token, char** argv, char* envp[]);
+char *getlyne(void);
+char *parser(char *buffer);
+char **arg_token(char *token);
+void exec(char *token, char **argv, char *envp[]);
 #endif
